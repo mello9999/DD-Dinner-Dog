3f2507ac73f9adf310fd67d53b5a376d29b99613
bank1333 <natchapol.pat@gmail.com>
2020-12-07 14:34:17 +0700

    add chatmay

---
+++ frontend/src/components/ChatMay/indxex.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import left  จากไหน;
+import right จากไหน;
+
+export default function ChatMay(){
+    
+    return (
+        <div>
+            <left></left>
+            <right></right>
+        </div>
+    );
+}
